fa3c58b8ad22fcb43474509a593d23894b1b580d
viraj2252 <viraj2252@gmail.com>
2016-11-01 23:31:34 +0800

    Test for correct parse

--- Youi.Parser.Test/CSVParserTest.cs
+++ Youi.Parser.Test/CSVParserTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Youi.Parser.FileParsers;
 using Youi.Parser.Interfaces;
@@ -17,7 +18,8 @@ namespace Youi.Parser.Test
         [TestInitialize]
         public void TestSetup()
         {
-           
+            DirectoryInfo di = new DirectoryInfo(Assembly.GetExecutingAssembly().FullName);
+            _correctTempPath = $"{di.Parent?.Parent?.Parent?.FullName}\\{_correctTempPath}";
         }
 
         [TestMethod]
@@ -26,6 +28,15 @@ namespace Youi.Parser.Test
         {
             _csvParser = new CsvParser(_wrongFilePath);
             _csvParser.ParseFile();
+        }
+
+        [TestMethod]
+        public void correct_path_should_return_8_records()
+        {
+            _csvParser = new CsvParser(_correctTempPath);
+            var result = _csvParser.ParseFile();
+
+            Assert.AreEqual(8, result.Count);
         }
     }
 }
--- Youi.Parser/FileParsers/CsvParser.cs
+++ Youi.Parser/FileParsers/CsvParser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Youi.Parser.Interfaces;
 using Youi.Parser.Models;
@@ -31,10 +32,34 @@ namespace Youi.Parser.FileParsers
         {
             FileInfo fi = new FileInfo(_filePath);
 
+            //Check whether the file exists. If not fire an exception
             if(!fi.Exists)
                 throw new FileNotFoundException("csv file does not exists under the give path!");
 
-            return null;
+            List<User> users = new List<User>();
+            var allLines = File.ReadAllLines(_filePath);
+
+            for (var i = 1; i < allLines.Length; i++)
+            {
+                var parts = allLines[i].Split(new char[] {','});
+                if(parts.Length < 4)
+                    continue;
+
+                var addressParts = Regex.Split(parts[2], "([0-9]+)");
+                users.Add(new User
+                {
+                    FirstName = parts[0],
+                    LastName = parts[1],
+                    Address = new Address
+                    {
+                        Number = Convert.ToInt32(addressParts[1]),
+                        Street = addressParts[2]
+                    },
+                    PhoneNumber = Convert.ToInt64(parts[3])
+                });
+            }
+
+            return users;
         }
 
         #endregion Interface Methods
